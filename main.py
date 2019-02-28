b0f6365603d33490263088120f57a9dbeedbd48d
Omer Belhasin <omerb01@gmail.com>
2019-02-28 18:42:19 +0200

    added parser

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("Lets rule them all!")
 
 print("I got 99 problems but commit ain't one")
+
+
+def parse_txt(path, func):
+    rows = []
+    with open(path, 'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            rows.append(func(line))
+    return rows
